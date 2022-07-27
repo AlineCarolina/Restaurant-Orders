12b89f1768487a1ba63441d8bd5a679232688cb6
Aline <eualine2017@gmail.com>
2022-07-27 17:10:21 -0300

    feat: implementa função read em analyze_log

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,19 @@
+import csv
+
+
+def read(path):
+    if not path.endswith(".csv"):
+        raise FileNotFoundError(f"Extensão inválida: {path}")
+    try:
+        data_csv = []
+        with open(path, "r", encoding="utf-8") as file:
+            for row in csv.reader(file):
+                data_csv.append(row)
+        return data_csv
+
+    except FileExistsError:
+        raise FileNotFoundError(f"Arquivo inexistente: {path}")
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    read(path_to_file)
