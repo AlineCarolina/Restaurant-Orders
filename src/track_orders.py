585590a0f0fb7f72d03cbd4175984c1e49d99ac2
Aline <eualine2017@gmail.com>
2022-07-28 16:39:43 -0300

    Requisito 2

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,19 +1,30 @@
+from src.analyze_log import (
+    most_requested_dish,
+    dish_never_requested,
+    day_without_order,
+)
+
+
 class TrackOrders:
-    # aqui deve expor a quantidade de estoque
+
+    def __init__(self):
+        self.orders = list()
+        self.days = dict()
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, customer, order, day):
-        pass
+        self.orders.append((customer, order, day))
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        return most_requested_dish(customer, self.orders)
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        return dish_never_requested(customer, self.orders)
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        return day_without_order(customer, self.orders)
 
     def get_busiest_day(self):
         pass
